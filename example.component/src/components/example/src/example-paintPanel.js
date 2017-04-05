42ec20db67bf20b68b5b7676580b4e3691d1ac49
Mikhail Sadouski <Mikhail_Sadouski@epam.com>
2017-04-05 23:33:11 +0300

    Add button to view semantic neighborhood

--- example.component/src/components/example/src/example-paintPanel.js
+++ example.component/src/components/example/src/example-paintPanel.js
@@ -15,6 +15,39 @@ Example.PaintPanel.prototype = {
     _initMarkup: function (containerId) {
         var container = $('#' + containerId);
 
+        var self = this;
         container.append('<div class="sc-no-default-cmd">Example Component</div>');
-    }
+        container.append('<button id="newButton" type="button">Узнать все о главном меню</button>');
+
+        $('#newButton').click(function () {
+			self._showMainMenuNode();
+		});
+    },
+
+    /* Call agent of searching semantic neighborhood,
+	send ui_main_menu node as parameter and add it in web window history
+	*/
+	_showMainMenuNode: function () {
+		var addr;
+		// Resolve sc-addr. Get sc-addr of ui_main_menu node
+		SCWeb.core.Server.resolveScAddr(['ui_main_menu'], function (keynodes) {
+			addr = keynodes['ui_main_menu'];
+			// Resolve sc-addr of ui_menu_view_full_semantic_neighborhood node
+			SCWeb.core.Server.resolveScAddr(["ui_menu_view_full_semantic_neighborhood"],
+			function (data) {
+				// Get command of ui_menu_view_full_semantic_neighborhood
+				var cmd = data["ui_menu_view_full_semantic_neighborhood"];
+				// Simulate click on ui_menu_view_full_semantic_neighborhood button
+				SCWeb.core.Main.doCommand(cmd,
+				[addr], function (result) {
+					// waiting for result
+					if (result.question != undefined) {
+						// append in history
+						SCWeb.ui.WindowManager.appendHistoryItem(result.question);
+					}
+				});
+			});
+		});
+	}
+    
 };
